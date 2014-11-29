accfede628507242dc5329514cbd1415a4b82238
Matej Urbas <matej.urbas@gmail.com>
2014-11-29 19:39:53 +0000

    Added task initialization.

---
+++ budApi/SettingsConstruction/Ops/InitializeTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Bud.SettingsConstruction.Ops {
+  public class InitializeTask<T> : Setting {
+    public Func<BuildConfiguration, T> InitialValue;
+
+    public InitializeTask(TaskKey<T> key, T initialValue) : this(key, b => initialValue) {}
+
+    public InitializeTask(TaskKey<T> key, Func<BuildConfiguration, T> initialValue) : base(key) {
+      this.InitialValue = initialValue;
+    }
+
+    public override void ApplyTo(ImmutableDictionary<ISettingKey, IValueDefinition>.Builder buildConfigurationBuilder) {
+      if (buildConfigurationBuilder.ContainsKey(Key)) {
+        throw new InvalidOperationException(string.Format("Cannot initialize the task '{0}'. It has already been initialized.", Key));
+      }
+      buildConfigurationBuilder[Key] = new TaskDefinition<T>(InitialValue);
+    }
+  }
+}
+
