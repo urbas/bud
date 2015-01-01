9b06797777f1c5522f14bd5769a88f0138395f04
Matej Urbas <matej.urbas@gmail.com>
2015-01-01 14:13:53 +0100

    Pulled plugin utils into a separate file.

--- Graph/Plugin.cs
+++ Graph/Plugin.cs
@@ -1,16 +1,6 @@
 using System;
 
 namespace Bud {
-  public static class PluginUtils {
-    public static IPlugin With(this IPlugin thisPlugin, IPlugin otherPlugin) {
-      return otherPlugin == null ? thisPlugin : thisPlugin.With(otherPlugin.ApplyTo);
-    }
-
-    public static IPlugin With(this IPlugin thisPlugin, SettingApplication settingApplication) {
-      return new Plugin((existingSettings, scope) => settingApplication(thisPlugin.ApplyTo(existingSettings, scope), scope));
-    }
-  }
-
   public class Plugin : IPlugin {
     public static readonly Plugin New = new Plugin();
 
---
+++ Graph/PluginUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bud {
+  public static class PluginUtils {
+    public static IPlugin With(this IPlugin thisPlugin, IPlugin otherPlugin) {
+      return otherPlugin == null ? thisPlugin : thisPlugin.With(otherPlugin.ApplyTo);
+    }
+
+    public static IPlugin With(this IPlugin thisPlugin, SettingApplication settingApplication) {
+      return new Plugin((existingSettings, scope) => settingApplication(thisPlugin.ApplyTo(existingSettings, scope), scope));
+    }
+  }
+}
+
