d6c84a3c18badf6c16ea45177d69ac94fcf6699c
Matej Urbas <matej.urbas@gmail.com>
2015-03-08 15:06:25 +0000

    Removed sln and csproj files from the repo. They are generated by Bud now.

--- Core/src/main/cs/BudPaths.cs
+++ Core/src/main/cs/BudPaths.cs
@@ -3,6 +3,6 @@ using System.IO;
 
 namespace Bud {
   public static class BudPaths {
-    public static readonly string GlobalConfigDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData ), "Bud");
+    public static readonly string GlobalConfigDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".bud");
   }
 }
