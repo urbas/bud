ad794568e0326d3e91a1539344aee902c5eb7129
Matej Urbas <matej.urbas@gmail.com>
2015-04-01 08:37:51 +0100

    Rewrote a test to use the new Project syntax.

--- SystemTests/src/test/budProjects/ProjectWithTests/.bud/Build.cs
+++ SystemTests/src/test/budProjects/ProjectWithTests/.bud/Build.cs
@@ -2,11 +2,10 @@ using Bud;
 using Bud.CSharp;
 using Bud.Projects;
 using System.IO;
-using System;
 
 public class Build : IBuild {
   public Settings Setup(Settings settings, string baseDir) {
-    return settings
-      .Project("A", Path.Combine(baseDir, "A"), Cs.Dll(), Cs.Test());
+    var projectA = new Project("A", Path.Combine(baseDir, "A"), Cs.Dll(), Cs.Test());
+    return settings.Add(projectA);
   }
 }
