8135ae79dc364e3d7fe4e76bc544de79c1589d11
Matej Urbas <matej.urbas@gmail.com>
2015-04-20 20:13:01 +0100

    Added a library project definition example snippet.

---
+++ Bud.Examples.Snippets/src/main/cs/CSharpProjectSnippets.cs
@@ -0,0 +1,13 @@
+﻿using Bud.CSharp;
+using Bud.Projects;
+
+namespace Bud.Examples.Snippets {
+  public static class CSharpProjectSnippets {
+    public static Settings LibraryProjectDefinition(Settings settings, string baseDir) {
+      // SNIPPET: libraryProjectDefinition
+      var libraryProject = new Project("Foo.Bar", baseDir, Cs.Dll());
+      return settings.Add(libraryProject);
+      // END_SNIPPET: libraryProjectDefinition
+    }
+  }
+}
