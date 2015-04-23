ab29581fdbf5b031517d52e93c7bce09689c1b2e
Matej Urbas <matej.urbas@gmail.com>
2015-04-23 11:37:01 +0100

    Prepared the release script.

--- DevelopmentUtils/prepare-release.py
+++ DevelopmentUtils/prepare-release.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from argparse import ArgumentParser
 from subprocess import call
 from os.path import join
@@ -8,6 +9,8 @@ from os.path import join
 CHOCOLATEY_PACKAGE_DIR='DevelopmentUtils/ChocolateyPackage'
 CHOCOLATEY_NUSPEC_FILE=join(CHOCOLATEY_PACKAGE_DIR, 'bud.nuspec')
 CHOCOLATEY_PS1_FILE=join(CHOCOLATEY_PACKAGE_DIR, 'tools', 'chocolateyInstall.ps1')
+BUD_DIST_DIR=join('bud', '.bud', 'output', 'main', 'cs', 'dist')
+
 
 def replace_in_file(file, regex_s_replacement):
   call(['sed', '-r', regex_s_replacement, '-i', file])
@@ -19,7 +22,7 @@ def update_nuspec_version(version):
 
 
 def update_dist_zip_url(version):
-  replacement='s/bud-.+?\\.zip/bud-{0}.zip/'.format(version)
+  replacement='s/bud-.+?\\.zip/{0}/'.format(bud_dist_zip_name(version))
   replace_in_file(CHOCOLATEY_PS1_FILE, replacement)
 
 
@@ -31,11 +34,49 @@ def git_tag_release(version):
   call(['git', 'tag', 'v{0}'.format(version)])
 
 
+def create_dist():
+  call(['bud', 'project/bud/clean'])
+  call(['bud', 'project/bud/main/cs/dist'])
+
+
+def bud_dist_zip_name(version):
+  return 'bud-{0}.zip'.format(version)
+
+
+def bud_choco_package_name(version):
+  return 'bud.{0}.nupkg'.format(version)
+
+
+def create_zip(version):
+  old_dir=os.getcwd()
+  os.chdir(BUD_DIST_DIR)
+  call(['zip', '-r', bud_dist_zip_name(version), '.'])
+  os.chdir(old_dir)
+
+
+def chocolatey_push(version):
+  old_dir=os.getcwd()
+  os.chdir(CHOCOLATEY_PACKAGE_DIR)
+  call(['cpack'])
+  call(['cpush', bud_choco_package_name(version)])
+  os.chdir(old_dir)
+
+
+def upload_package(version):
+  package_name=bud_dist_zip_name(version)
+  package_relative_path=join('.', BUD_DIST_DIR, package_name)
+  call(['scp', '-i', '~/.ssh/budpage_id_rsa', package_relative_path, 'budpage@54.154.215.159:/home/budpage/production-budpage/shared/public/packages/{0}'.format(package_name)])
+
+
 def prepare_release(version):
   update_nuspec_version(version)
   update_dist_zip_url(version)
   git_release_commit(version)
   git_tag_release(version)
+  create_dist()
+  create_zip(version)
+  upload_package(version)
+  chocolatey_push(version)
 
 
 def run():
--- RELEASE_INSTRUCTIONS.md
+++ RELEASE_INSTRUCTIONS.md
@@ -2,24 +2,8 @@ Preparation:
 
 - Install the API key (has to be done in administrative console): `choco apiKey -k <your-api-key-here> -source https://chocolatey.org/`
 
-Release steps:
-
-1. Set the release version and prepare the release: 
+- Invoke this python script: 
 
     ```
-    VER=0.1.2 && \
-    sed -r "s/(<version>).+(<\/version>)/\1$VER\2/" -i chocolatey/bud.nuspec && \
-    sed -r "s/(bud-).+?(.zip)/\1$VER\2/" -i chocolatey/tools/chocolateyInstall.ps1 && \
-    git commit -am "Release $VER." && \
-    git tag v$VER
+    ./DevelopmentUtils/prepare-release.py x.y.z
     ```
-
-1. Invoke `bud project/bud/main/cs/dist`.
-
-1. Create a zip of the contents of the `bud\.bud\output\main\cs\dist` folder. The name of the zip file should be `bud-x.x.x.zip`
-
-1. Place the zip into `/home/budpage/production-budpage/shared/public/packages`.
-
-1. Go to the folder `chocolatey` and run `cpack` there.
-
-1. Upload manually for now on chocolatey. Now push the package with `choco push bud.x.x.x.nupkg`
