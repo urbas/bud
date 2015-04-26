e66bef7d9c7e3994943edfde142671984a60df96
Matej Urbas <matej.urbas@gmail.com>
2015-04-26 10:43:08 +0100

    Projects' base directories can now be omitted.

--- Bud.SystemTests/src/test/budProjects/InterProjectDependencies/.bud/Build.cs
+++ Bud.SystemTests/src/test/budProjects/InterProjectDependencies/.bud/Build.cs
@@ -5,8 +5,9 @@ using System.IO;
 
 public class Build : IBuild {
   public Settings Setup(Settings settings, string baseDir) {
-    return settings
-      .Project("A", Path.Combine(baseDir, "A"), Cs.Dll())
-      .Project("B", Path.Combine(baseDir, "B"), Cs.Exe(Cs.Dependency("A")));
+    var projectA = new Project("A", Cs.Dll());
+
+    return settings.Add(projectA)
+                   .Project("B", Path.Combine(baseDir, "B"), Cs.Exe(Cs.Dependency("A")));
   }
 }
