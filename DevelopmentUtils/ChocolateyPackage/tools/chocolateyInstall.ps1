8aba4a7a3e7e47fa3388fb59c17fd54b09569515
Matej Urbas <matej.urbas@gmail.com>
2015-04-28 18:57:34 +0100

    Setting next development version: 0.2.1-dev

--- DevelopmentUtils/ChocolateyPackage/tools/chocolateyInstall.ps1
+++ DevelopmentUtils/ChocolateyPackage/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'bud' 'http://bud.urbas.si/packages/bud-0.2.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'bud' 'http://bud.urbas.si/packages/bud-0.2.1-dev.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
