aca4de4e0eef9530f2d172da388ac59aef3f2134
Matej Urbas <matej.urbas@gmail.com>
2015-10-05 15:32:40 +0100

    Moved private classes into Tasks.

--- Bud.Core/src/main/cs/Tasking/ITaskModification.cs
+++
@@ -1,7 +0,0 @@
-namespace Bud.Tasking {
-  internal interface ITaskModification {
-    string Name { get; }
-    TaskDefinition Modify(TaskDefinition taskDefinition);
-    TaskDefinition ToTaskDefinition();
-  }
-}
--- Bud.Core/src/main/cs/Tasking/Tasks.cs
+++ Bud.Core/src/main/cs/Tasking/Tasks.cs
@@ -31,9 +31,10 @@ namespace Bud.Tasking {
     /// </exception>
     /// <typeparam name="T">the requested type of the task's result.</typeparam>
     public Task<T> Get<T>(Key<T> taskName) => new TasksResultCache(Compile()).Get(taskName);
+
     public Tasks Set<T>(Key<T> taskName, Func<ITasks, Task<T>, Task<T>> task) => Add(new TaskModification<T>(taskName, task));
     public Tasks ExtendWith(Tasks tasks) => new Tasks(TaskModifications.Concat(tasks.TaskModifications));
-    public Tasks Nest(string prefix) => new Tasks(TaskModifications.Select(taskModification => new TaskNestingModification(prefix, taskModification)));
+    public Tasks Nest(string prefix) => new Tasks(TaskModifications.Select(taskModification => new TaskNesting(prefix, taskModification)));
 
     internal IDictionary<string, TaskDefinition> Compile() {
       var taskDefinitions = new Dictionary<string, TaskDefinition>();
@@ -48,6 +49,67 @@ namespace Bud.Tasking {
       return taskDefinitions;
     }
 
-    internal Tasks Add(ITaskModification taskModification) => new Tasks(TaskModifications.Concat(new [] {taskModification}));
+    private Tasks Add(ITaskModification taskModification) => new Tasks(TaskModifications.Concat(new[] {taskModification}));
+
+    private interface ITaskModification {
+      string Name { get; }
+      TaskDefinition Modify(TaskDefinition taskDefinition);
+      TaskDefinition ToTaskDefinition();
+    }
+
+    private class TaskModification<T> : ITaskModification {
+      public string Name { get; }
+      private Func<ITasks, Task<T>, Task<T>> Task { get; }
+
+      public TaskModification(Key<T> name, Func<ITasks, Task<T>, Task<T>> task) {
+        Name = name;
+        Task = task;
+      }
+
+      public TaskDefinition Modify(TaskDefinition taskDefinition) {
+        TasksExtensions.AssertTaskTypeIsSame<T>(Name, taskDefinition);
+        return new TaskDefinition(typeof(T), tasks => Task(tasks, (Task<T>) taskDefinition.Task(tasks)));
+      }
+
+      public TaskDefinition ToTaskDefinition() => new TaskDefinition(typeof(T), tasks => Task(tasks, null));
+    }
+
+    private class TaskNesting : ITaskModification {
+      public string Name { get; }
+      private string Prefix { get; }
+      private ITaskModification NestedModification { get; }
+
+      public TaskNesting(string prefix, ITaskModification nestedModification) {
+        Prefix = prefix;
+        NestedModification = nestedModification;
+        Name = prefix + "/" + nestedModification.Name;
+      }
+
+      public TaskDefinition Modify(TaskDefinition taskDefinition) {
+        var modifiedTaskDefinition = NestedModification.Modify(taskDefinition);
+        return new TaskDefinition(modifiedTaskDefinition.ReturnType, tasks => {
+          var prefixedTasks = new NestedTasks(Prefix, tasks);
+          return modifiedTaskDefinition.Task(prefixedTasks);
+        });
+      }
+
+      public TaskDefinition ToTaskDefinition() {
+        var taskDefinition = NestedModification.ToTaskDefinition();
+        return new TaskDefinition(taskDefinition.ReturnType, tasks => taskDefinition.Task(new NestedTasks(Prefix, tasks)));
+      }
+    }
+
+    private class NestedTasks : ITasks {
+      private readonly string prefix;
+      private readonly ITasks tasks;
+
+      public NestedTasks(string prefix, ITasks tasks) {
+        this.prefix = prefix + "/";
+        this.tasks = tasks;
+      }
+
+      public Task<T> Get<T>(Key<T> taskName) => tasks.Get<T>(prefix + taskName);
+      public Task Get(Key taskName) => tasks.Get(prefix + taskName);
+    }
   }
 }
