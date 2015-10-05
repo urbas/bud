40115d8774f54ce9923237b2d57922b91b955a9d
Matej Urbas <matej.urbas@gmail.com>
2015-10-05 21:49:33 +0100

    Moar removal.

--- Bud.Core/src/main/cs/Resources/Res.cs
+++
@@ -1,8 +0,0 @@
-﻿using Bud.Build;
-
-namespace Bud.Resources {
-  public static class Res {
-    public static Plugin Main(params Setup[] setups) => new ResourcesBuildTarget(BuildKeys.Main, setups);
-    public static Plugin Test(params Setup[] setups) => new ResourcesBuildTarget(BuildKeys.Test, setups);
-  }
-}
