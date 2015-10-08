36bbd181f8f7b41de92201d6daa0ccc389faeb38
Matej Urbas <matej.urbas@gmail.com>
2015-10-08 09:02:04 +0100

    Extracted the subdirectory exclusion filter.

--- Bud.Test/CSharpTest.cs
+++ Bud.Test/CSharpTest.cs
@@ -12,6 +12,7 @@ using Moq;
 using NUnit.Framework;
 using static Bud.Build;
 using static Bud.CSharp;
+using static Bud.NuGet;
 
 namespace Bud {
   public class CSharpTest {
@@ -35,15 +36,19 @@ namespace Bud {
     public async void Compile_bud() {
       await Project(@"C:\Users\matej\Programming\bud\Bud")
         .ExtendWith(SourceDir(fileFilter: "*.cs"))
-        .Modify(Sources, (configs, previousFiles) => {
-          var forbiddenDirs = new [] {"obj", "bin", "target"}.Select(s => Path.Combine(ProjectDir[configs], s));
-          return new FilterFiles(previousFiles, file => !forbiddenDirs.Any(file.StartsWith));
-        })
+        .ExtendWith(ExcludeSourceDirs("obj", "bin", "target"))
         .ExtendWith(CSharpCompilation())
-        .ExtendWith(NuGet.NuGetPackages())
+        .ExtendWith(NuGetPackages())
         .Get(Compile)
         .Do(result => Console.WriteLine($"\n=== COMPILING =============================\nCompiled: {result.AssemblyPath}\nSuccess: {result.EmitResult.Success} \nDiagnostics: {string.Join("\n", result.EmitResult.Diagnostics.Select(diagnostic => diagnostic.ToString()))}\n================================"))
         .ToTask();
     }
+
+    private static Configs ExcludeSourceDirs(params string[] subDirs)
+      => Configs.Empty
+                .Modify(Sources, (configs, previousFiles) => {
+                  var forbiddenDirs = subDirs.Select(s => Path.Combine(ProjectDir[configs], s));
+                  return new FilterFiles(previousFiles, file => !forbiddenDirs.Any(file.StartsWith));
+                });
   }
 }
