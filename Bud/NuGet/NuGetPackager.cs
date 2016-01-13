5ed9a8697fad1a3d8b03bada277880e064ce292e
Matej Urbas <matej.urbas@gmail.com>
2016-01-13 09:28:31 +0000

    Specifies files in the NuSpec file rather than copying them around.

--- Bud/NuGet/NuGetPackager.cs
+++ Bud/NuGet/NuGetPackager.cs
@@ -8,38 +8,24 @@ using static Bud.NuGet.NuGetExecutable;
 namespace Bud.NuGet {
   public class NuGetPackager : IPackager {
     public string Pack(string outputDir, string packageId, string version, IEnumerable<PackageFile> files, IEnumerable<PackageDependency> packageDependencies, NuGetPackageMetadata packageMetadata) {
-      var filesDir = Combine(outputDir, "files");
-      var nupkgDir = Combine(outputDir, "nupkg");
       var nuspecFile = Combine(outputDir, $"{packageId}.nuspec");
-      var nupkgFile = Combine(nupkgDir, $"{packageId}.{version}.nupkg");
-      CreateDirectory(filesDir);
-      CreateDirectory(nupkgDir);
-      CopyPackageFiles(files, filesDir);
-      CreateNuspecFile(nuspecFile, packageId, version, packageDependencies, packageMetadata);
+      CreateDirectory(outputDir);
+      CreateNuspecFile(nuspecFile, packageId, version, packageDependencies, packageMetadata, files);
       ExecuteNuGet($"pack {nuspecFile} " +
-                   $"-OutputDirectory {nupkgDir} " +
-                   $"-BasePath {filesDir}");
-      return nupkgFile;
+                   $"-OutputDirectory {outputDir} " +
+                   $"-BasePath {outputDir}");
+      return Combine(outputDir, $"{packageId}.{version}.nupkg");
     }
 
-    private static void CopyPackageFiles(IEnumerable<PackageFile> files, string filesDir) {
-      Delete(filesDir, true);
-      foreach (var packageFile in files) {
-        var destFileName = Combine(filesDir, packageFile.PathInPackage);
-        CreateDirectory(GetDirectoryName(destFileName));
-        Copy(packageFile.FileToPackage, destFileName, true);
-      }
-    }
-
-    private static void CreateNuspecFile(string nuspecFile, string packageId, string version, IEnumerable<PackageDependency> packageDependencies, NuGetPackageMetadata packageMetadata) {
+    private static void CreateNuspecFile(string nuspecFile, string packageId, string version, IEnumerable<PackageDependency> packageDependencies, NuGetPackageMetadata packageMetadata, IEnumerable<PackageFile> files) {
       using (var stream = Open(nuspecFile, FileMode.Create, FileAccess.Write)) {
         using (var writer = new StreamWriter(stream)) {
-          WriteNuspecContent(writer, packageId, version, packageDependencies, packageMetadata);
+          WriteNuspecContent(writer, packageId, version, packageDependencies, packageMetadata, files);
         }
       }
     }
 
-    private static void WriteNuspecContent(TextWriter writer, string packageId, string version, IEnumerable<PackageDependency> packageDependencies, NuGetPackageMetadata packageMetadata) {
+    private static void WriteNuspecContent(TextWriter writer, string packageId, string version, IEnumerable<PackageDependency> packageDependencies, NuGetPackageMetadata packageMetadata, IEnumerable<PackageFile> files) {
       writer.Write("<?xml version=\"1.0\"?>");
       writer.Write("<package xmlns=\"http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd\">");
       writer.Write("<metadata>");
@@ -56,6 +42,11 @@ namespace Bud.NuGet {
       }
       writer.Write("</dependencies>");
       writer.Write("</metadata>");
+      writer.Write("<files>");
+      foreach (var file in files) {
+        writer.Write($"<file src=\"{file.FileToPackage}\" target=\"{file.PathInPackage}\" />");
+      }
+      writer.Write("</files>");
       writer.Write("</package>");
     }
   }
