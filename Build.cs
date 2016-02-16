14670c71bea8bc36c1a050a1978b26aee0b85b71
Matej Urbas <matej.urbas@gmail.com>
2016-02-16 18:33:56 +0000

    Renames Bud to bud.

--- Build.cs
+++ Build.cs
@@ -10,8 +10,8 @@ using static Bud.V1.Api;
 
 public class BudBuild : IBuild {
   public Conf Init()
-    => Projects(CsApp("Bud")
+    => Projects(CsApp("bud")
                   .SetValue(ProjectVersion, "0.5.0-pre-1"),
                 CsLibrary("Bud.Test")
-                  .Add(Dependencies, "../Bud"));
+                  .Add(Dependencies, "../bud"));
 }
--- bud/Properties/AssemblyInfo.cs
+++ bud/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Bud")]
+[assembly: AssemblyTitle("bud")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bud")]
+[assembly: AssemblyProduct("bud")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
