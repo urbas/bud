f57d5f4b42c02034a0bb951057125b337120c1ae
Matej Urbas <matej.urbas@gmail.com>
2016-03-03 09:04:45 +0000

    Adds Exec.RunCheckedQuietly.

--- bud/Cli/Exec.cs
+++ bud/Cli/Exec.cs
@@ -3,14 +3,21 @@ using System.Diagnostics;
 
 namespace Bud.Cli {
   public static class Exec {
+    /// <summary>
+    ///   Runs the command, suppresses all its output, and throws an exception
+    ///   if it returns a no-zero error code.
+    /// </summary>
+    public static void RunCheckedQuietly(string executablePath, string arguments) {
+      var process = CreateWindowlessProcess(executablePath, arguments);
+      process.Start();
+      process.WaitForExit();
+      if (process.ExitCode != 0) {
+        throw new Exception($"The command '{executablePath}' with arguments '{arguments}' returned an error code of {process.ExitCode}.");
+      }
+    }
+
     public static int Run(string executablePath, string arguments) {
-      var process = new Process();
-      process.StartInfo = new ProcessStartInfo(executablePath, arguments) {
-        CreateNoWindow = true,
-        UseShellExecute = false,
-        RedirectStandardOutput = true,
-        RedirectStandardError = true
-      };
+      var process = CreateWindowlessProcess(executablePath, arguments);
       process.OutputDataReceived += ProcessOnOutputDataReceived;
       process.Start();
       process.BeginOutputReadLine();
@@ -18,6 +25,16 @@ namespace Bud.Cli {
       process.WaitForExit();
       return process.ExitCode;
     }
+
+    private static Process CreateWindowlessProcess(string executablePath, string arguments)
+      => new Process {
+        StartInfo = new ProcessStartInfo(executablePath, arguments) {
+          CreateNoWindow = true,
+          UseShellExecute = false,
+          RedirectStandardOutput = true,
+          RedirectStandardError = true
+        }
+      };
 
     private static void ReadErrorOutput(Process process) {
       var errorOutput = process.StandardError.ReadToEnd();
@@ -27,7 +44,10 @@ namespace Bud.Cli {
     }
 
     private static void ProcessOnOutputDataReceived(object sender,
-                                                    DataReceivedEventArgs outputLine)
-      => Console.WriteLine(outputLine.Data);
+                                                    DataReceivedEventArgs outputLine) {
+      if (outputLine.Data != null) {
+        Console.WriteLine(outputLine.Data);
+      }
+    }
   }
 }
