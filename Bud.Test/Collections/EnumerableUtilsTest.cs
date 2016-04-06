2901669890d372367c3aa55adf41c92aa128dbf3
Matej Urbas <matej.urbas@gmail.com>
2016-04-06 23:28:11 +0100

    Adds EnumerableUtils.TryGetFirst.

--- Bud.Test/Collections/EnumerableUtilsTest.cs
+++ Bud.Test/Collections/EnumerableUtilsTest.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using NUnit.Framework;
 using static Bud.Collections.EnumerableUtils;
+using static Bud.Util.Option;
+using static NUnit.Framework.Assert;
 
 namespace Bud.Collections {
   public class EnumerableUtilsTest {
@@ -9,8 +13,20 @@ namespace Bud.Collections {
       var collection1 = new[] {42, 9001};
       var collection2 = ImmutableArray.CreateRange(collection1);
 
-      Assert.AreEqual(collection1, collection2);
-      Assert.AreEqual(ElementwiseHashCode(collection1), ElementwiseHashCode(collection2));
+      AreEqual(collection1, collection2);
+      AreEqual(ElementwiseHashCode(collection1), ElementwiseHashCode(collection2));
     }
+
+    [Test]
+    public void TryGetFirst_returns_none_when_enumerable_is_empty()
+      => AreEqual(None<int>(), Enumerable.Empty<int>().TryGetFirst());
+
+    [Test]
+    public void TryGetFirst_returns_the_first_element_when_enumerable_is_not_empty()
+      => AreEqual(Some(42), new [] {42, 1}.TryGetFirst());
+
+    [Test]
+    public void TryGetFirst_returns_None_when_given_null()
+      => AreEqual(None<int>(), (null as IEnumerable<int>).TryGetFirst());
   }
 }
--- bud/Collections/EnumerableUtils.cs
+++ bud/Collections/EnumerableUtils.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Bud.Util;
+using static Bud.Util.Option;
 
 namespace Bud.Collections {
   public static class EnumerableUtils {
     public static int ElementwiseHashCode<TElement>(IEnumerable<TElement> enumerable)
       => enumerable?.Aggregate(487, MergeHash) ?? 0;
 
+    public static Option<TSource> TryGetFirst<TSource>(this IEnumerable<TSource> source) {
+      if (source == null) {
+        return None<TSource>();
+      }
+      var list = source as IList<TSource>;
+      if (list != null) {
+        if (list.Count > 0) {
+          return list[0];
+        }
+      } else {
+        using (var e = source.GetEnumerator()) {
+          if (e.MoveNext()) {
+            return e.Current;
+          }
+        }
+      }
+      return None<TSource>();
+    }
+
     private static int MergeHash<TElement>(int hashCode, TElement element) {
       unchecked {
-        return hashCode * 31 + element.GetHashCode();
+        return hashCode*31 + element.GetHashCode();
       }
     }
   }
