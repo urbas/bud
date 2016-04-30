f26194c1ef3a6ff45eb85194a17d84d09c8f52fb
Matej Urbas <matej.urbas@gmail.com>
2016-04-30 19:53:18 +0100

    Applies cosmetics.

--- bud/Configuration/PerKeyConf.cs
+++ bud/Configuration/PerKeyConf.cs
@@ -4,10 +4,6 @@ using Bud.V1;
 
 namespace Bud.Configuration {
   internal class PerKeyConf : IConf {
-    private IImmutableList<string> Dir { get; }
-    private IConf Conf { get; }
-    private ConfCache Cache { get; }
-
     internal PerKeyConf(IConf conf, IImmutableList<string> dir, Key key) {
       Dir = dir;
       Conf = conf;
@@ -16,11 +12,10 @@ namespace Bud.Configuration {
     }
 
     public Key Key { get; }
-
-    public Option<T> TryGet<T>(Key<T> key)
-      => Cache.TryGet(key, RawTryGet);
-
-    private Option<T> RawTryGet<T>(Key<T> key)
-      => Conf.TryGet<T>(Keys.InterpretFromDir(key.Id, Dir));
+    public Option<T> TryGet<T>(Key<T> key) => Cache.TryGet(key, RawTryGet);
+    private Option<T> RawTryGet<T>(Key<T> key) => Conf.TryGet<T>(Keys.InterpretFromDir(key.Id, Dir));
+    private IImmutableList<string> Dir { get; }
+    private IConf Conf { get; }
+    private ConfCache Cache { get; }
   }
 }
