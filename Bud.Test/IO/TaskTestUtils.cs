6a13f8e0b536a9b5b857efdb33ec8fd1ade328e0
Matej Urbas <matej.urbas@gmail.com>
2016-05-14 22:10:10 +0100

    Adds some background task invocation utilities for testing.

---
+++ Bud.Test/IO/TaskTestUtils.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+
+namespace Bud.IO {
+  public static class TaskTestUtils {
+    public static T InvokeAndWait<T>(Func<CountdownEvent, T> invokee,
+                                     int waitCountdown = 1) {
+      var barrier = new CountdownEvent(waitCountdown);
+      var observedChanges = invokee(barrier);
+      barrier.Wait();
+      return observedChanges;
+    }
+
+    public static void InvokeAndWait(Action<CountdownEvent> invokee,
+                                     int waitCountdown = 1) {
+      var barrier = new CountdownEvent(waitCountdown);
+      invokee(barrier);
+      barrier.Wait();
+    }
+  }
+}
