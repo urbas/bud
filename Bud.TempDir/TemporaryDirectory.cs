e8ae79d9a1a267ea2a03f96857cbe05d91764254
Matej Urbas <matej.urbas@gmail.com>
2016-05-22 09:52:29 +0100

    TemporaryDirectory now at least tries to delete the dir before showing open files. Adds documentation to TemporaryDirectory.

--- Bud.TempDir/TemporaryDirectory.cs
+++ Bud.TempDir/TemporaryDirectory.cs
@@ -13,45 +13,76 @@ namespace Bud.TempDir {
     public string Path { get; }
 
     public void Dispose() {
-      var openedFiles = FindOpenedFiles();
-      if (openedFiles.Count > 0) {
-        throw new Exception($"Could not delete the temporary directory {Path}. There were some locked files: {string.Join(", ", openedFiles)}");
+      try {
+        Directory.Delete(Path, true);
+      } catch (Exception e) {
+        var openedFiles = FindOpenedFiles();
+        if (openedFiles.Count > 0) {
+          throw new Exception($"Could not delete the temporary directory {Path}. There were some locked files: {string.Join(", ", openedFiles)}", e);
+        }
+        throw;
       }
-      Directory.Delete(Path, true);
     }
 
-    public List<string> FindOpenedFiles()
-      => Directory.EnumerateFiles(Path, "*", SearchOption.AllDirectories).Where(IsFileLocked)
-                                                               .ToList();
-
-    public override string ToString() => Path;
-
+    /// <param name="targetPath">
+    ///   a list of path components. These are combined to produce the path of the file to create.
+    /// </param>
+    /// <returns>the path of the created file</returns>
     public string CreateEmptyFile(params string[] targetPath)
       => CreateFile(string.Empty, targetPath);
 
+    /// <param name="content">the content of the file to create.</param>
+    /// <param name="targetPath">
+    ///   a list of path components. These are combined to produce the path of the file to create.
+    /// </param>
+    /// <returns>the path of the created file</returns>
     public string CreateFile(string content, params string[] targetPath) {
       var file = CreateFilePathAndDir(targetPath);
       File.WriteAllText(file, content);
       return file;
     }
 
+    /// <param name="subDirPath">
+    ///   a list of path components. These are combined to produce the path of the directory to create.
+    /// </param>
+    /// <returns>the path of the created directory.</returns>
+    public string CreateDir(params string[] subDirPath) {
+      var newDir = System.IO.Path.Combine(new[] {Path}.Concat(subDirPath).ToArray());
+      Directory.CreateDirectory(newDir);
+      return newDir;
+    }
+
+    /// <summary>
+    ///   creates a file with the contents of the given embedded resource.
+    /// </summary>
+    /// <param name="resourceName">the name of the embedded resource file.</param>
+    /// <param name="targetPath">
+    ///   a list of path components. These are combined to produce the path of the file to create.
+    /// </param>
+    /// <returns>the path of the created file</returns>
     public string CreateFileFromResource(string resourceName, params string[] targetPath) {
       using (var packagesConfigStream = Assembly.GetCallingAssembly().GetManifestResourceStream(resourceName)) {
         return CreateFile(packagesConfigStream, targetPath);
       }
     }
 
+    /// <summary>
+    ///   Returns a path within this tempororary directory without creating a file.
+    /// </summary>
+    /// <param name="targetPath">
+    ///   a list of path components. These are combined to produce the path within the temporary directory.
+    /// </param>
+    /// <returns>the path.</returns>
     public string CreatePath(params string[] targetPath)
       => targetPath.Length == 0 ?
            System.IO.Path.Combine(Path, Guid.NewGuid().ToString()) :
            System.IO.Path.Combine(new[] {Path}.Concat(targetPath).ToArray());
 
-    private string CreateFilePathAndDir(string[] targetPath) {
-      var file = CreatePath(targetPath);
-      Directory.CreateDirectory(System.IO.Path.GetDirectoryName(file));
-      return file;
-    }
-
+    /// <param name="content">the content the new file should contain.</param>
+    /// <param name="targetPath">
+    ///   a list of path components. These are combined to produce the path of the file to create.
+    /// </param>
+    /// <returns>the path of the created file</returns>
     public string CreateFile(Stream content, params string[] targetPath) {
       var file = CreateFilePathAndDir(targetPath);
       using (var fileStream = File.Open(file, FileMode.Create, FileAccess.Write)) {
@@ -60,10 +91,19 @@ namespace Bud.TempDir {
       return file;
     }
 
-    public string CreateDir(params string[] subDirPath) {
-      var newDir = System.IO.Path.Combine(new[] {Path}.Concat(subDirPath).ToArray());
-      Directory.CreateDirectory(newDir);
-      return newDir;
+    /// <returns>a list of all open files in this temporary directory.</returns>
+    public List<string> FindOpenedFiles()
+      => Directory.EnumerateFiles(Path, "*", SearchOption.AllDirectories)
+                  .Where(IsFileLocked)
+                  .ToList();
+
+    /// <returns>the path of this temporary directory.</returns>
+    public override string ToString() => Path;
+
+    private string CreateFilePathAndDir(string[] targetPath) {
+      var file = CreatePath(targetPath);
+      Directory.CreateDirectory(System.IO.Path.GetDirectoryName(file));
+      return file;
     }
 
     private static string CreateDirectoryInTempDir() {
