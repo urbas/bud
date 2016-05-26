cb97c62adff868ea35ec26f17bb55ae5bfd1b627
Matej Urbas <matej.urbas@gmail.com>
2016-05-26 20:32:58 +0100

    Script runner now prints error details without a stack trace to stderr.

--- Bud.Scripting/ScriptRunner.cs
+++ Bud.Scripting/ScriptRunner.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Bud.Scripting {
   public class ScriptRunner {
     /// <summary>
@@ -7,8 +9,18 @@ namespace Bud.Scripting {
     /// <param name="args">arguments that will be passed to the script's main method.</param>
     /// <param name="cwd">the working directory in which to run the script.</param>
     public static void Run(string scriptPath, string[] args, string cwd) {
-      var executable = ScriptBuilder.Build(scriptPath);
-      BatchExec.Run(executable, string.Join(" ", args), cwd);
+      string executable = null;
+      try {
+        executable = ScriptBuilder.Build(scriptPath);
+        BatchExec.Run(executable, string.Join(" ", args), cwd);
+      } catch (Exception e) {
+        if (executable == null) {
+          Console.Error.WriteLine("An error occurred while building the script. Error message:\n\n    " + e.Message);
+          Environment.Exit(1);
+        } else {
+          throw;
+        }
+      }
     }
   }
 }
