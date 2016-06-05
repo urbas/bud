ca2ddef66970d3a0d825e0449374595eee6cbfea
Matej Urbas <matej.urbas@gmail.com>
2016-06-05 14:29:25 +0100

    Removes the dependency on Immutable Collections from Bud.Building.

--- Bud.Building/TimestampBasedBuilder.cs
+++ Bud.Building/TimestampBasedBuilder.cs
@@ -1,30 +1,33 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 
 namespace Bud.Building {
   public class TimestampBasedBuilder {
+    private static readonly IReadOnlyList<string> EmptyInputFiles = new ReadOnlyCollection<string>(new string[0]);
+
     /// <summary>
     ///   Builds a single file with the given list of inputs.
     /// </summary>
     /// <param name="fileGenerator">this function actually produces the output.</param>
     /// <param name="output">the path of the expected output.</param>
-    /// <param name="inputFiles">the files from which the <paramref name="fileGenerator"/> should generate the output.</param>
+    /// <param name="inputFiles">the files from which the <paramref name="fileGenerator" /> should generate the output.</param>
     /// <remarks>
-    ///   The worker will not be invoked if the last-modified timestamp of the <paramref name="output"/> is newer
+    ///   The worker will not be invoked if the last-modified timestamp of the <paramref name="output" /> is newer
     ///   than the last-modified timestamp of any of the input files.
     /// </remarks>
     public static void Build(FilesBuilder fileGenerator,
                              string output,
                              IReadOnlyList<string> inputFiles) {
-      inputFiles = inputFiles ?? ImmutableList<string>.Empty;
+      inputFiles = inputFiles ?? EmptyInputFiles;
       if (!File.Exists(output) || AnyFileNewer(inputFiles, output)) {
         fileGenerator(inputFiles, output);
       }
     }
 
-    private static bool AnyFileNewer(IEnumerable<string> files, string referenceFile) {
+    private static bool AnyFileNewer(IEnumerable<string> files,
+                                     string referenceFile) {
       var timestampOfReference = File.GetLastWriteTimeUtc(referenceFile);
       return files.Any(file => File.GetLastWriteTimeUtc(file) > timestampOfReference);
     }
