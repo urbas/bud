f0bca6fc533fd861e7be989354958c33e42f2297
Matej Urbas <matej.urbas@gmail.com>
2016-10-09 12:03:19 +0100

    Extracts string-building blocks in CsProj into their own functions. Moves CsProjElement into its own file.

--- Libs/Bud.CsProjTools/CsProj.cs
+++ Libs/Bud.CsProjTools/CsProj.cs
@@ -1,49 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Bud.CsProjTools {
   public static class CsProj {
-    private const string Indentation = "  ";
-    public static readonly IEnumerable<KeyValuePair<string, string>> NoAttributes = Enumerable.Empty<KeyValuePair<string, string>>();
+    public const string Indentation = "  ";
+    public static readonly IEnumerable<Tuple<string, string>> NoAttributes = Enumerable.Empty<Tuple<string, string>>();
     public static readonly IEnumerable<CsProjElement> NoElements = Enumerable.Empty<CsProjElement>();
 
     public static string Generate(params CsProjElement[] elements) {
       var stringBuilder = new StringBuilder();
       stringBuilder.Append(@"<?xml version=""1.0"" encoding=""utf-8""?>").Append('\n');
       stringBuilder.Append(@"<Project ToolsVersion=""14.0"" DefaultTargets=""Build"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">").Append('\n');
-      foreach (var element in elements) {
-        Append(stringBuilder, element, Indentation);
-        stringBuilder.Append('\n');
-      }
+      AppendElements(stringBuilder, elements, Indentation);
       stringBuilder.Append(@"</Project>");
       return stringBuilder.ToString();
-    }
-
-    private static void Append(StringBuilder stringBuilder, CsProjElement element, string indentantion) {
-      stringBuilder.Append(indentantion)
-        .Append($@"<{element.Name}");
-      foreach (var attribute in element.Attributes) {
-        stringBuilder.Append($@" {attribute.Key}=""{attribute.Value}""");
-      }
-      if (element.Content.HasValue || element.Children.Any()) {
-        stringBuilder.Append(">");
-        if (element.Children.Any()) {
-          stringBuilder.Append('\n');
-          var nextIndentation = indentantion + Indentation;
-          foreach (var child in element.Children) {
-            Append(stringBuilder, child, nextIndentation);
-            stringBuilder.Append('\n');
-          }
-          stringBuilder.Append(indentantion);
-        }
-        if (element.Content.HasValue) {
-          stringBuilder.Append(element.Content.Value);
-        }
-        stringBuilder.Append($@"</{element.Name}>");
-      } else {
-        stringBuilder.Append(" />");
-      }
     }
 
     public static CsProjElement Import(string project, Option<string> condition = default(Option<string>))
@@ -61,47 +33,71 @@ namespace Bud.CsProjTools {
                            ToConditionAttribute(condition),
                            properties);
 
-    public static CsProjElement Property(string key, string value,
+    public static CsProjElement Property(string key,
+                                         string value,
                                          Option<string> condition = default(Option<string>))
       => new CsProjElement(key, ToConditionAttribute(condition), NoElements, value);
 
-    public static IEnumerable<KeyValuePair<string, string>> AsAttributes(string key, string project)
-      => new[] {new KeyValuePair<string, string>(key, project)};
+    public static IEnumerable<Tuple<string, string>> AsAttributes(string key, string project)
+      => new[] {new Tuple<string, string>(key, project)};
 
-    public static IEnumerable<CsProjElement> AsElements(CsProjElement element)
-      => new[] {element};
+    public static IEnumerable<CsProjElement> AsElements(params CsProjElement[] elements)
+      => elements;
 
-    private static IEnumerable<KeyValuePair<string, string>> ToConditionAttribute(Option<string> condition)
-      => condition.Map(c => AsAttributes("Condition", c))
-                  .GetOrElse(NoAttributes);
+    private static IEnumerable<Tuple<string, string>> ToConditionAttribute(Option<string> condition)
+      => condition.Map(c => AsAttributes("Condition", c)).GetOrElse(NoAttributes);
 
     public static CsProjElement ItemGroup(params CsProjElement[] elements)
-      => new CsProjElement("ItemGroup", CsProj.NoAttributes, elements);
+      => new CsProjElement("ItemGroup", NoAttributes, elements);
 
     public static CsProjElement Reference(string assemblyName, Option<string> hintPath = default(Option<string>))
       => Item("Reference", assemblyName, hintPath);
 
     public static CsProjElement Item(string itemName, string includeName, Option<string> hintPath = default(Option<string>))
       => new CsProjElement(itemName,
-                           CsProj.AsAttributes("Include", includeName),
-                           hintPath.Map(p => CsProj.AsElements(new CsProjElement("HintPath", CsProj.NoAttributes, CsProj.NoElements, p)))
-                                   .GetOrElse(CsProj.NoElements));
-  }
+                           AsAttributes("Include", includeName),
+                           hintPath.Map(p => AsElements(new CsProjElement("HintPath", NoAttributes, NoElements, p)))
+                                   .GetOrElse(NoElements));
 
-  public class CsProjElement {
-    public string Name { get; }
-    public Option<string> Content { get; }
-    public IEnumerable<CsProjElement> Children { get; }
-    public IEnumerable<KeyValuePair<string, string>> Attributes { get; }
+    private static void AppendElement(StringBuilder stringBuilder, CsProjElement element, string indentation) {
+      stringBuilder.Append(indentation).Append($@"<{element.Name}");
+      AppendAttributes(stringBuilder, element.Attributes);
+      if (element.Content.HasValue || element.Children.Any()) {
+        stringBuilder.Append(">");
+        AppendChildren(stringBuilder, element, indentation);
+        if (element.Content.HasValue) {
+          stringBuilder.Append(element.Content.Value);
+        }
+        stringBuilder.Append($@"</{element.Name}>");
+      } else {
+        stringBuilder.Append(" />");
+      }
+    }
 
-    public CsProjElement(string name,
-                         IEnumerable<KeyValuePair<string, string>> attributes,
-                         IEnumerable<CsProjElement> children,
-                         Option<string> content = default(Option<string>)) {
-      Attributes = attributes.ToList();
-      Name = name;
-      Children = children;
-      Content = content;
+    private static void AppendAttributes(StringBuilder stringBuilder,
+                                         IEnumerable<Tuple<string, string>> attributes) {
+      foreach (var attribute in attributes) {
+        stringBuilder.Append($@" {attribute.Item1}=""{attribute.Item2}""");
+      }
+    }
+
+    private static void AppendChildren(StringBuilder stringBuilder,
+                                       CsProjElement element,
+                                       string indentation) {
+      if (element.Children.Any()) {
+        stringBuilder.Append('\n');
+        AppendElements(stringBuilder, element.Children, indentation + Indentation);
+        stringBuilder.Append(indentation);
+      }
+    }
+
+    private static void AppendElements(StringBuilder stringBuilder,
+                                       IEnumerable<CsProjElement> elements,
+                                       string indentation) {
+      foreach (var child in elements) {
+        AppendElement(stringBuilder, child, indentation);
+        stringBuilder.Append('\n');
+      }
     }
   }
 }
---
+++ Libs/Bud.CsProjTools/CsProjElement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bud.CsProjTools {
+  public class CsProjElement {
+    public string Name { get; }
+    public Option<string> Content { get; }
+    public IEnumerable<CsProjElement> Children { get; }
+    public IEnumerable<Tuple<string, string>> Attributes { get; }
+
+    public CsProjElement(string name,
+                         IEnumerable<Tuple<string, string>> attributes,
+                         IEnumerable<CsProjElement> children,
+                         Option<string> content = default(Option<string>)) {
+      Attributes = attributes.ToList();
+      Name = name;
+      Children = children;
+      Content = content;
+    }
+  }
+}
